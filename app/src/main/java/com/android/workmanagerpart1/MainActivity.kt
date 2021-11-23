738604fcd1c68eea0ae731e327d4e1f0449664d0
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-11-23 17:03:00 +0700

    work manager one time request

--- app/src/main/java/com/android/workmanagerpart1/MainActivity.kt
+++ app/src/main/java/com/android/workmanagerpart1/MainActivity.kt
@@ -2,10 +2,35 @@ package com.android.workmanagerpart1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
 
 class MainActivity : AppCompatActivity() {
+
+    private var btnStart:Button?=null
+    private var txtHasil:TextView?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnStart = findViewById(R.id.btnStart)
+        txtHasil = findViewById(R.id.txtHasil)
+
+        btnStart?.setOnClickListener {
+            setOnTimeWorkRequest()
+        }
+
+    }
+
+    private fun setOnTimeWorkRequest(){
+        val uploadRequest = OneTimeWorkRequest
+            .Builder(UploadWorker::class.java)
+            .build()
+
+        WorkManager.getInstance(applicationContext)
+            .enqueue(uploadRequest)
     }
 }
---
+++ app/src/main/java/com/android/workmanagerpart1/UploadWorker.kt
@@ -0,0 +1,21 @@
+package com.android.workmanagerpart1
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class UploadWorker(context: Context,params: WorkerParameters): Worker(context,params) {
+    override fun doWork(): Result {
+        try {
+            for (i in 0..600000){
+                Log.i("MYTAG","Uploading $i")
+            }
+
+            return Result.success()
+
+        }catch (e:Exception){
+            return Result.failure()
+        }
+    }
+}
