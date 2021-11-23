6140000166eec1f160e4ee1afc5ec81cf9b52d47
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-11-23 17:17:39 +0700

    implement constraint baterry dan internet

--- app/src/main/java/com/android/workmanagerpart1/MainActivity.kt
+++ app/src/main/java/com/android/workmanagerpart1/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.Observer
+import androidx.work.Constraints
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 
@@ -30,8 +32,15 @@ class MainActivity : AppCompatActivity() {
 
         val workManager = WorkManager.getInstance(applicationContext)
 
+        //constraint
+        val constraint = Constraints.Builder()
+            .setRequiresCharging(true)
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
         val uploadRequest = OneTimeWorkRequest
             .Builder(UploadWorker::class.java)
+            .setConstraints(constraint)
             .build()
 
         workManager.enqueue(uploadRequest)
